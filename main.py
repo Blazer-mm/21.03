c4406f06b7cd1d149503b6dd3d78a262730543ae
Anna Fiscenkova <91870533+Blazer-mm@users.noreply.github.com>
2022-03-21 08:34:15 +0000

    idk?

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ b = int(input("b: "))
 c = int(input("c: "))
 D = b**2-4*a*c
 if D<0:
-  print("nav saknes")
+  print("nav saknes, diskriminants < 0")
 elif D>0:
   x1 = (-b+sqrt(D))/2*a
   x2 = (-b-sqrt(D))/2*a
+  print("divas saknes, diskriminants > 0")
   print(x1,x2)
 elif D==0:
   x1 = (-b+sqrt(D))/2*a
+  print("viena sakne, diskriminants = 0")
   print(x1)
